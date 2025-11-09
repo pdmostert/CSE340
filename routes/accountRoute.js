be309f14a6fd1805267dd7ced33294135ad8c50e
Peter Mostert <peter@pdm-pix.com>
2025-11-09 17:36:15 +0200

    Implement registration validation; add validation rules and error handling for registration form

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -11,7 +12,13 @@ router.post("/login", utilities.handleErrors(accountController.loginAccount));
 
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 
 module.exports = router;
